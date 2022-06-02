0623ee8372f1d167a4d9dc1a4495e3f9c7c1ea53
cjh1249131356 <cjh1249131356@gmail.com>
2022-06-02 22:26:30 +0800

    GameSpace: Long click Settings icon to open Settings Dashboard
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBarService.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBarService.kt
@@ -41,6 +41,7 @@ import com.android.systemui.screenrecord.IRecordingCallback
 import dagger.hilt.android.AndroidEntryPoint
 import io.chaldeaprjkt.gamespace.R
 import io.chaldeaprjkt.gamespace.data.AppSettings
+import io.chaldeaprjkt.gamespace.settings.SettingsActivity
 import io.chaldeaprjkt.gamespace.utils.ScreenUtils
 import io.chaldeaprjkt.gamespace.utils.dp
 import io.chaldeaprjkt.gamespace.utils.registerDraggableTouchListener
@@ -352,6 +353,10 @@ class GameBarService : Hilt_GameBarService() {
         actionPanel.alpha = appSettings.menuOpacity / 100f
         actionPanel.setOnClickListener {
             showPanel = !showPanel
+        }
+        actionPanel.setOnLongClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+            true
         }
     }
 
