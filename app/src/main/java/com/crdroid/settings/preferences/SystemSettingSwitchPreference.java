4fcf4175c0006f5ab5dda73d357edecbb671ff90
Dmitrii <bankersenator@gmail.com>
2025-06-28 12:34:13 +0000

    GameSpace: crdroid -> voltage
    Change-Id: I58fd8cd9d02711f7274619a2c1f198ea3ebf29cd
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- app/src/main/java/com/crdroid/settings/preferences/SystemSettingSwitchPreference.java
+++ app/src/main/java/com/crdroid/settings/preferences/SystemSettingSwitchPreference.java
@@ -13,14 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.crdroid.settings.preferences;
+package com.voltage.support.preferences;
 
 import android.content.Context;
 import android.provider.Settings;
 import android.os.UserHandle;
 import android.util.AttributeSet;
 
-import lineageos.preference.SelfRemovingSwitchPreference;
+import com.voltage.support.preferences.SelfRemovingSwitchPreference;
 
 public class SystemSettingSwitchPreference extends SelfRemovingSwitchPreference {
 
